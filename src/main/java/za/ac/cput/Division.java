d17b6377592e8ae2fdfa174a1fc8540d7be3727c
gjjantjies9402 <80212402+gjjantjies9402@users.noreply.github.com>
2021-04-01 23:45:47 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/Division.java
@@ -0,0 +1,15 @@
+package za.ac.cput;
+/* Gaynor Jantjies
+    213069555
+    01/04/2021
+ */
+public class Division
+{
+
+   public int dVsn(int Num1,int Num2)
+   {
+    return Num1/Num2;
+     }
+
+
+}
