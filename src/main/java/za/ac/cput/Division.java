192897cee3cb9d0c3ba6c48792a5d4d225c068a9
gjjantjies9402 <80212402+gjjantjies9402@users.noreply.github.com>
2021-04-01 23:35:07 +0200

    Add files via upload

---
+++ src/main/java/za/ac/cput/Division.java
@@ -0,0 +1,17 @@
+package za.ac.cput;
+public class Division
+{
+   public int Num1 = 0;
+   public int Num2 = 0;
+   public int Num3;
+
+   public void Dvsn(int Num1,int Num2)
+   {
+    Num3= Num1/Num2;
+     }
+    /* public void Dvsn()
+     {
+      Num3 = Num1/Num2;
+       }*/
+
+}
